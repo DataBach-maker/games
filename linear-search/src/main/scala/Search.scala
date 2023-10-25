2f2fb26a4cb0a0ed16ab1ae26c5d54f10a71d208
Ariane <ariane.horbach@dpdhl.com>
2023-10-25 09:11:44 +0200

    [SA] Adding Binary Search

--- linear-search/src/main/scala/Search.scala
+++ linear-search/src/main/scala/Search.scala
@@ -1,6 +1,8 @@
 object Search extends App {
 
-  def linear(term: Array[Int], searchTerm: Int): Any = {
+  // first attempt: could be more functional
+  def linar_noCheatCode(term: Array[Int], searchTerm: Int): Any = {
+
     var foundValueAndIndex        = (0, 0);
     var couldNotFindValueAndIndex = "Could not be found"
 
@@ -9,14 +11,58 @@ object Search extends App {
       if (rowIndex == searchTerm) {
         foundValueAndIndex = (rowIndex, columnIndex)
         return foundValueAndIndex
+
       } else if (rowIndex != searchTerm && columnIndex == term.length - 1) {
         return couldNotFindValueAndIndex
       }
     }
 
   }
+  def linear_optimized(term: Array[Int], searchTerm: Int): Any = {
 
-  def binary(term: Array[Int], searchTerm: Int): Any = {
-    println(term)
+    term.zipWithIndex.find(_._1 == searchTerm) match {
+      case Some((value, index)) => (value, index)
+      case None                 => "Could not be found"
+    }
+
+  }
+
+  def binary_noCheatCode(term: Array[Int], searchTerm: Int): Any = {
+
+    val termLength: Int = term.length;
+
+    // binary Search assumes the array is sorted
+    val sortedTerm = term.sorted;
+
+    var leftIndex: Int  = 0;
+    var rightIndex: Int = termLength;
+
+    var foundValueAndIndex        = (0, 0);
+    var couldNotFindValueAndIndex = "Could not be found";
+
+    while (leftIndex <= rightIndex) {
+      val middleIndex: Int = leftIndex + (rightIndex - leftIndex) / 2
+
+      if (sortedTerm(middleIndex) == searchTerm) {
+        // substract 1 from middleIndex, because index arrays start at 0
+        foundValueAndIndex = (searchTerm, middleIndex - 1)
+        return foundValueAndIndex
+
+      } else if (sortedTerm(middleIndex) < searchTerm) {
+        leftIndex = middleIndex + 1
+
+      } else {
+        rightIndex = middleIndex - 1
+      }
+
+    }
+    couldNotFindValueAndIndex
+  }
+
+  def binary_optimized(term: Array[Int], searchTerm: Int): Any = {
+    val sortedTerm = term.sorted
+    val index      = java.util.Arrays.binarySearch(sortedTerm, searchTerm)
+    // binarySearch returns negative index if the value is not found
+    if (index >= 0) (searchTerm, index) else "Could not be found"
   }
 }
--- linear-search/src/test/scala/SearchSpec.scala
+++ linear-search/src/test/scala/SearchSpec.scala
@@ -1,18 +1,45 @@
-class Searches extends org.scalatest.funsuite.AnyFunSuite {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-  var term = Array(-1, 2, 3, 100, 2, 0)
+class SearchSpec extends AnyWordSpec with Matchers {
+
+  var term                   = Array(-1, 2, 3, 100, 5, 0)
   var searchTermSuccess: Int = 2
   var searchTermFailure: Int = -2
 
-  test("Search.linear") {
-    val success = Search.linear(term, searchTermSuccess)
-    assert(success == (2,1))
+  "Search" should {
 
-    val failure = Search.linear(term, searchTermFailure)
-    assert(failure == "Could not be found")
-  }
+    "return the correct result for no-cheat Code linear search" in {
+      val success = Search.linar_noCheatCode(term, searchTermSuccess)
+      success shouldEqual (2, 1)
 
-  test("Search.binary") {
-    Search.binary(term, searchTermSuccess)
+      val failure = Search.linar_noCheatCode(term, searchTermFailure)
+      failure shouldEqual "Could not be found"
+    }
+
+    "return the correct result for no-cheat Code binary search" in {
+      val success = Search.binary_noCheatCode(term, searchTermSuccess)
+      success shouldEqual (2, 1)
+
+      val failure = Search.binary_noCheatCode(term, searchTermFailure)
+      failure shouldEqual "Could not be found"
+    }
+
+    "return the correct result for optimized linear search" in {
+      val success = Search.linear_optimized(term, searchTermSuccess)
+      success shouldEqual (2, 1)
+
+      val failure = Search.linear_optimized(term, searchTermFailure)
+      failure shouldEqual "Could not be found"
+    }
+
+    "return the correct result for optimized binary search" in {
+      val success = Search.linear_optimized(term, searchTermSuccess)
+      success shouldEqual (2, 1)
+
+      val failure = Search.linear_optimized(term, searchTermFailure)
+      failure shouldEqual "Could not be found"
+    }
+
   }
 }
